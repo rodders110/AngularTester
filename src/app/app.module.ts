aa4894f8d270f625a0f58ab7adb95ad93ee5a7f1
Roddy Daly <roddy_daly110@hotmail.com>
2019-02-17 00:29:45 +0000

    test app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TesterComponent } from './tester/tester.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TesterComponent
   ],
   imports: [
     BrowserModule,
